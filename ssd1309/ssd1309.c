0ff906815888b75fb13bf3451ac486d5d58e1bab
Duy Lion Tran <lowe.tran@gmail.com>
2021-11-14 16:50:42 +0700

    Update latest features from afiskon and temporary remove the rotation feature

--- ssd1309/ssd1309.c
+++ ssd1309/ssd1309.c
@@ -75,10 +75,25 @@ void ssd1309_WriteData(uint8_t *buffer, size_t buff_size)
 #endif
 
 /* Screenbuffer */
-static uint8_t SSD1309_Buffer[SSD1309_WIDTH * SSD1309_HEIGHT / 8];
+static uint8_t SSD1309_Buffer[SSD1309_BUFFER_SIZE];
 
 /* Screen object */
 static SSD1309_t SSD1309;
+
+
+/* Fills the Screenbuffer with values from a given buffer of a fixed length */
+SSD1309_Error_t ssd1309_FillBuffer(uint8_t *buf, uint32_t len)
+{
+    SSD1309_Error_t ret = SSD1309_ERR;
+
+    if (len <= SSD1309_BUFFER_SIZE)
+    {
+        memcpy(SSD1309_Buffer, buf, len);
+        ret = SSD1309_OK;
+    }
+
+    return ret;
+}
 
 
 /* Initialize the oled screen */
@@ -139,10 +154,25 @@ void ssd1309_Init(ssd1309_spi_handle spi_comm_handle)
     ssd1309_WriteCommand(0xA6); /*--set normal color */
 #endif
 
-    ssd1309_WriteCommand(0xA8); /*--set multiplex ratio(1 to 64) - CHECK  */
-    ssd1309_WriteCommand(0x3F); /*                                        */
+/* Set multiplex ratio. */
+#if (SSD1309_HEIGHT == 128)
+    /* Found in the Luma Python lib for SH1106. */
+    ssd1306_WriteCommand(0xFF);
+#else
+    ssd1309_WriteCommand(0xA8); /*--set multiplex ratio(1 to 64) - CHECK */
+#endif
 
-    ssd1309_WriteCommand(0xA4); /* 0xA4,Output follows RAM content;0xa5,Output ignores RAM content */
+#if (SSD1309_HEIGHT == 32)
+    ssd1309_WriteCommand(0x1F);
+#elif (SSD1309_HEIGHT == 64)
+    ssd1309_WriteCommand(0x3F);
+#elif (SSD1309_HEIGHT == 128)
+    ssd1309_WriteCommand(0x3F); /* Seems to work for 128px high displays too. */
+#else
+#error "Only 32, 64, or 128 lines of height are supported!"
+#endif
+
+    ssd1309_WriteCommand(0xA4); /* 0xA4, Output follows RAM content;0xa5,Output ignores RAM content */
 
     ssd1309_WriteCommand(0xD3); /*-set display offset - CHECK */
     ssd1309_WriteCommand(0x00); /*-not offset */
@@ -154,7 +184,15 @@ void ssd1309_Init(ssd1309_spi_handle spi_comm_handle)
     ssd1309_WriteCommand(0x22); /*			  */
 
     ssd1309_WriteCommand(0xDA); /*--set com pins hardware configuration - CHECK */
+#if (SSD1309_HEIGHT == 32)
+    ssd1309_WriteCommand(0x02);
+#elif (SSD1309_HEIGHT == 64)
     ssd1309_WriteCommand(0x12);
+#elif (SSD1309_HEIGHT == 128)
+    ssd1309_WriteCommand(0x12);
+#else
+#error "Only 32, 64, or 128 lines of height are supported!"
+#endif
 
     ssd1309_WriteCommand(0xDB); /*--set vcomh */
     ssd1309_WriteCommand(0x20); /* 0x20, 0.77xVcc */ 
@@ -172,10 +210,11 @@ void ssd1309_Init(ssd1309_spi_handle spi_comm_handle)
     /* Set default values for screen object */
     SSD1309.CurrentX = 0;
     SSD1309.CurrentY = 0;
-    SSD1309.Rotated  = 0;
+    SSD1309.Rotation = ROTATION_0;
     
     SSD1309.Initialized = 1; 
 }
+
 
 /* Fill the whole screen with the given color */
 void ssd1309_Fill(SSD1309_COLOR color) 
@@ -192,13 +231,19 @@ void ssd1309_Fill(SSD1309_COLOR color)
 /* Write the screenbuffer with changed to the screen */
 void ssd1309_UpdateScreen(void) 
 {
-    uint8_t i;
-    for(i = 0; i < 8; i++) 
+    /* Write data to each page of RAM. Number of pages
+     * depends on the screen height:
+     *
+     *  * 32px   ==  4 pages
+     *  * 64px   ==  8 pages
+     *  * 128px  ==  16 pages 
+     */
+    for (uint8_t i = 0; i < (SSD1309_HEIGHT / 8); i++) 
     {
         ssd1309_WriteCommand(0xB0 + i);
         ssd1309_WriteCommand(0x00);
         ssd1309_WriteCommand(0x10);
-        ssd1309_WriteData(&SSD1309_Buffer[SSD1309_WIDTH*i],SSD1309_WIDTH);
+        ssd1309_WriteData(&SSD1309_Buffer[SSD1309_WIDTH * i], SSD1309_WIDTH);
     }
 }
 
@@ -208,20 +253,14 @@ void ssd1309_UpdateScreen(void)
 /*    color => Pixel color		  */
 void ssd1309_DrawPixel(uint8_t x, uint8_t y, SSD1309_COLOR color) 
 {
-    if((x >= SSD1309_WIDTH) || (y >= SSD1309_HEIGHT)) 
+    if ((x >= SSD1309_WIDTH) || (y >= SSD1309_HEIGHT)) 
     {
         /* Don't write outside the buffer */
         return;
     }
     
-    /* Check if pixel should be inverted */
-    if(SSD1309.Inverted) 
-    {
-        color = (SSD1309_COLOR)!color;
-    }
-    
     /* Draw in the right color */
-    if(color == White) 
+    if (color == White) 
     {
 	SSD1309_Buffer[x + (y / 8) * SSD1309_WIDTH] |= 1 << (y % 8);
     } 
@@ -238,27 +277,20 @@ void ssd1309_DrawPixel(uint8_t x, uint8_t y, SSD1309_COLOR color)
 char ssd1309_WriteChar(char ch, FontDef Font, SSD1309_COLOR color) 
 {
     uint32_t i, b, j;
+
+    /* Check if character is valid */
+    if (ch < 32 || ch > 126)
+    {
+        return 0;
+    }
     
     /* Check remaining space on current line */
-    if (0 == SSD1309.Rotated)
+    if ((SSD1309_WIDTH <= (SSD1309.CurrentX + Font.FontWidth))  ||
+        (SSD1309_HEIGHT <= (SSD1309.CurrentY + Font.FontHeight))
+       )
     {
-	if ((SSD1309_WIDTH <= (SSD1309.CurrentX + Font.FontWidth))  ||
-	    (SSD1309_HEIGHT <= (SSD1309.CurrentY + Font.FontHeight))
-	   )
-	{
-	    /* Not enough space on current line */
-	    return 0;
-	}
-    }
-    else
-    {
-	if ((SSD1309_WIDTH <= (SSD1309.CurrentX - Font.FontWidth))  ||
-	    (SSD1309_HEIGHT <= (SSD1309.CurrentY - Font.FontHeight))
-	   )
-	{
-	    /* Not enough space on current line */
-	    return 0;
-	}
+        /* Not enough space on current line */
+        return 0;
     }
     
     /* Use the font to write */
@@ -270,25 +302,11 @@ char ssd1309_WriteChar(char ch, FontDef Font, SSD1309_COLOR color)
 	{
             if ((b << j) & 0x8000)  
 	    {
-		if (0 == SSD1309.Rotated)
-		{
-		    ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)color);
-		}
-		else
-		{
-                    ssd1309_DrawPixel(SSD1309.CurrentX - j, (SSD1309.CurrentY - i), (SSD1309_COLOR)color);
-		}
+                ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)color);
             } 
 	    else 
 	    {
-		if (0 == SSD1309.Rotated)
-		{
-		    ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)!color);
-                }
-		else
-		{
-                    ssd1309_DrawPixel(SSD1309.CurrentX - j, (SSD1309.CurrentY - i), (SSD1309_COLOR)!color);
-		}
+                ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)!color);
             }
         }
     }
@@ -309,25 +327,12 @@ void ssd1309_WriteSymbol(SymbolID_t Symbol, uint8_t x, uint8_t y)
     ssd1309_SetCursor(x, y);
     
     /* Check remaining space on current line */
-    if (0 == SSD1309.Rotated)
+    if ((SSD1309_WIDTH <= (SSD1309.CurrentX + SSD1309_Symbol[Symbol].SymbolWidth))  ||
+        (SSD1309_HEIGHT <= (SSD1309.CurrentY + SSD1309_Symbol[Symbol].SymbolHeight))
+       )
     {
-	if ((SSD1309_WIDTH <= (SSD1309.CurrentX + SSD1309_Symbol[Symbol].SymbolWidth))  ||
-	    (SSD1309_HEIGHT <= (SSD1309.CurrentY + SSD1309_Symbol[Symbol].SymbolHeight))
-	   )
-	{
-	    /* Not enough space on current line */
-	    return ;
-	}
-    }
-    else
-    {
-	if ((SSD1309_WIDTH <= (SSD1309.CurrentX - SSD1309_Symbol[Symbol].SymbolWidth))  ||
-	    (SSD1309_HEIGHT <= (SSD1309.CurrentY - SSD1309_Symbol[Symbol].SymbolHeight))
-	   )
-	{
-	    /* Not enough space on current line */
-	    return ;
-	}
+        /* Not enough space on current line */
+        return ;
     }
 
     /* Use the data to write */
@@ -339,25 +344,11 @@ void ssd1309_WriteSymbol(SymbolID_t Symbol, uint8_t x, uint8_t y)
 	{
             if ((b << j) & 0x8000)  
 	    {
-		if (0 == SSD1309.Rotated)
-		{
-		    ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)color);
-		}
-		else
-		{
-                    ssd1309_DrawPixel(SSD1309.CurrentX - j, (SSD1309.CurrentY - i), (SSD1309_COLOR)color);
-		}
+                ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)color);
             } 
 	    else 
 	    {
-		if (0 == SSD1309.Rotated)
-		{
-		    ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)!color);
-                }
-		else
-		{
-                    ssd1309_DrawPixel(SSD1309.CurrentX - j, (SSD1309.CurrentY - i), (SSD1309_COLOR)!color);
-		}
+                ssd1309_DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), (SSD1309_COLOR)!color);
             }
         }
     }
@@ -366,38 +357,6 @@ void ssd1309_WriteSymbol(SymbolID_t Symbol, uint8_t x, uint8_t y)
     SSD1309.CurrentX += SSD1309_Symbol[Symbol].SymbolWidth;
 }
 
-
-void reverseString(char* str) 
-{ 
-    int l, i; 
-    char *begin_ptr, *end_ptr, ch; 
-  
-    // Get the length of the string 
-    l = strlen(str); 
-  
-    // Set the begin_ptr and end_ptr 
-    // initially to start of string 
-    begin_ptr = str; 
-    end_ptr = str; 
-  
-    // Move the end_ptr to the last character 
-    for (i = 0; i < l - 1; i++) 
-        end_ptr++; 
-  
-    // Swap the char from start and end 
-    // index using begin_ptr and end_ptr 
-    for (i = 0; i < l / 2; i++) { 
-  
-        // swap character 
-        ch = *end_ptr; 
-        *end_ptr = *begin_ptr; 
-        *begin_ptr = ch; 
-  
-        // update pointers positions 
-        begin_ptr++; 
-        end_ptr--; 
-    } 
-}
 
 /* Write full string to screenbuffer */
 char ssd1309_WriteString(char* str, FontDef Font, SSD1309_COLOR color) 
@@ -408,81 +367,242 @@ char ssd1309_WriteString(char* str, FontDef Font, SSD1309_COLOR color)
 
     CLEAR_BUFFER(data);
 
-    if (0 != SSD1309.Rotated)
+    /* Write until null-byte */
+    while (*str) 
     {
-	while (*str)
-	{
-	    data[data_counter] = *str;
-	    data_counter++;
-            str++;
-	}
-    }
-
-    if (0 == SSD1309.Rotated)
-    {
-	/* Write until null-byte */
-	while (*str) 
-	{
-	    if (ssd1309_WriteChar(*str, Font, color) != *str) 
-	    {
-		/* Char could not be written */
-		return *str;
-	    }
-        
-	    /* Next char */
-	    str++;
-	}
-    }
-    else
-    {
-	for (print_counter = 0; print_counter < data_counter; print_counter++)
-	{
-	    if (ssd1309_WriteChar(data[(data_counter - 1) - print_counter], Font, color) != data[(data_counter - 1) - print_counter]) 
-	    {
-		/* Char could not be written */
-		return data[(data_counter - 1) - print_counter];
-	    }
-	}
+        if (ssd1309_WriteChar(*str, Font, color) != *str) 
+        {
+            /* Char could not be written */
+            return *str;
+        }
+    
+        /* Next char */
+        str++;
     }
     
     /* Everything ok */
     return *str;
 }
 
+
 /* Position the cursor */
 void ssd1309_SetCursor(uint8_t x, uint8_t y) 
 {
-    if (0 == SSD1309.Rotated)
+    SSD1309.CurrentX = x - SSD1309_OFFSET_X;
+    SSD1309.CurrentY = y - SSD1309_OFFSET_Y;
+}
+
+
+/* Draw line by Bresenhem's algorithm */
+void ssd1309_DrawLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1309_COLOR color)
+{
+    int32_t deltaX = abs(x2 - x1);
+    int32_t deltaY = abs(y2 - y1);
+    int32_t signX = ((x1 < x2) ? 1 : -1);
+    int32_t signY = ((y1 < y2) ? 1 : -1);
+    int32_t error = deltaX - deltaY;
+    int32_t error2;
+
+    ssd1309_DrawPixel(x2, y2, color);
+    while ((x1 != x2) || (y1 != y2))
     {
-	SSD1309.CurrentX = x - SSD1309_OFFSET_X;
-	SSD1309.CurrentY = y - SSD1309_OFFSET_Y;
+        ssd1309_DrawPixel(x1, y1, color);
+        error2 = error * 2;
+
+        if (error2 > -deltaY)
+        {
+            error -= deltaY;
+            x1 += signX;
+        }
+        else
+        {
+            /* Nothing to do */    
+        }
+    
+        if (error2 < deltaX)
+        {
+            error += deltaX;
+            y1 += signY;
+        }
+        else
+        {
+            /* Nothing to do */
+        }
+    }
+    return;
+}
+
+
+/* DrawArc. Draw angle is beginning from 4 quart of trigonometric circle (3pi/2)
+ * start_angle in degree
+ * sweep in degree
+ */
+void ssd1309_DrawArc(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep, SSD1309_COLOR color)
+{
+    #define CIRCLE_APPROXIMATION_SEGMENTS 36
+    float approx_degree;
+    uint32_t approx_segments;
+    uint8_t xp1,xp2;
+    uint8_t yp1,yp2;
+    uint16_t loc_sweep = 0;
+    uint16_t loc_angle_count = 0;
+    uint32_t count = 0;
+    
+    float rad;
+    
+    normalize2_0_360(sweep, &loc_sweep);
+    normalize2_0_360(start_angle, &loc_angle_count);
+    
+    count = (loc_angle_count * CIRCLE_APPROXIMATION_SEGMENTS) / 360;
+    approx_segments = (loc_sweep * CIRCLE_APPROXIMATION_SEGMENTS) / 360;
+    approx_degree = loc_sweep / (float)approx_segments;
+
+    while (count < approx_segments)
+    {
+        degree2radian(count * approx_degree, &rad);
+        xp1 = x + (int8_t)(sin(rad) * radius);
+        yp1 = y + (int8_t)(cos(rad) * radius);    
+        count++;
+
+        if (count != approx_segments)
+        {
+            degree2radian(count * approx_degree, &rad);
+        }
+        else
+        {            
+            degree2radian(loc_sweep, &rad);
+        }
+
+        xp2 = x + (int8_t)(sin(rad) * radius);
+        yp2 = y + (int8_t)(cos(rad) * radius);    
+        ssd1309_DrawLine(xp1, yp1, xp2, yp2, color);
+    }
+}
+
+
+/* Draw circle by Bresenhem's algorithm */
+void ssd1309_DrawCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, SSD1309_COLOR color)
+{
+    int32_t x = -par_r;
+    int32_t y = 0;
+    int32_t err = 2 - 2 * par_r;
+    int32_t e2;
+
+    if (par_x >= SSD1309_WIDTH || par_y >= SSD1309_HEIGHT)
+    {
+        return;
+    }
+
+    do {
+        ssd1309_DrawPixel(par_x - x, par_y + y, color);
+        ssd1309_DrawPixel(par_x + x, par_y + y, color);
+        ssd1309_DrawPixel(par_x + x, par_y - y, color);
+        ssd1309_DrawPixel(par_x - x, par_y - y, color);
+        e2 = err;
+
+        if (e2 <= y)
+        {
+            y++;
+            err = err + (y * 2 + 1);
+
+            if (-x == y && e2 <= x)
+            {
+                e2 = 0;
+            }
+            else
+            {
+                /* Nothing to do */
+            }
+        }
+        else
+        {
+            /* Nothing to do */
+        }
+
+        if (e2 > x)
+        {
+            x++;
+            err = err + (x * 2 + 1);
+        }
+        else
+        {
+            /* Nothing to do */
+        }
+    } while (x <= 0);
+
+    return;
+}
+
+
+/* Draw polyline */
+void ssd1309_Polyline(const SSD1309_VERTEX *par_vertex, uint16_t par_size, SSD1309_COLOR color)
+{
+    uint16_t i;
+
+    if (par_vertex != NULL)
+    {
+        for (i = 1; i < par_size; i++)
+        {
+            ssd1309_DrawLine(par_vertex[i - 1].x, par_vertex[i - 1].y, par_vertex[i].x, par_vertex[i].y, color);
+        }
     }
     else
     {
-	SSD1309.CurrentX = SSD1309_WIDTH - x + SSD1309_OFFSET_X;
-	SSD1309.CurrentY = SSD1309_HEIGHT - 1 - y + SSD1309_OFFSET_Y;
+        /* Nothing to do */
     }
+    return;
 }
 
 
-bool ssd1309_Rotated(void)
+/* Draw rectangle */
+void ssd1309_DrawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1309_COLOR color)
 {
-    if (0 == SSD1309.Rotated)
+    ssd1309_DrawLine(x1, y1, x2, y1, color);
+    ssd1309_DrawLine(x2, y1, x2, y2, color);
+    ssd1309_DrawLine(x2, y2, x1, y2, color);
+    ssd1309_DrawLine(x1, y2, x1, y1, color);
+}
+
+
+/* Draw bitmap - ported from the ADAFruit GFX library */
+void ssd1309_DrawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, SSD1309_COLOR color)
+{
+    uint8_t byte = 0;
+    int16_t byteWidth = (w + 7) / 8; /* Bitmap scanline pad = whole byte */
+
+    if (x >= SSD1309_WIDTH || y >= SSD1309_HEIGHT)
     {
-	return false;
+        return;
     }
 
-    return true;
+    for (uint8_t j = 0; j < h; j++, y++)
+    {
+        for (uint8_t i = 0; i < w; i++)
+        {
+            if (i & 7)
+            {
+                byte <<= 1;
+            }
+            else
+            {
+                byte = (*(const unsigned char *)(&bitmap[j * byteWidth + i / 8]));
+            }
+
+            if (byte & 0x80)
+            {
+                ssd1309_DrawPixel(x + i, y, color);
+            }
+        }
+    }
+
+    return;
 }
 
 
-void ssd1309_RotatedText(void)
+void ssd1309_SetContrast(const uint8_t value)
 {
-    SSD1309.Rotated = 1;
-}
+    const uint8_t kSetContrastControlRegister = 0x81;
 
-
-void ssd1309_NonrotatedText(void)
-{
-    SSD1309.Rotated = 0;
+    ssd1309_WriteCommand(kSetContrastControlRegister);
+    ssd1309_WriteCommand(value);
 }
--- ssd1309/ssd1309.h
+++ ssd1309/ssd1309.h
@@ -10,8 +10,8 @@
 #ifndef __SSD1309_H__
 #define __SSD1309_H__
 
+#include <math.h>
 #include <stddef.h>
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
@@ -48,7 +48,7 @@ typedef void (*ssd1309_spi_handle)(uint8_t, uint8_t *, size_t);
 
 /* SSD1309 width in pixels	  */
 #ifndef SSD1309_WIDTH
-#define SSD1309_WIDTH           128
+#define SSD1309_WIDTH           130
 #endif
 
 /* SSD1309 offset of x in pixels  */
@@ -60,6 +60,8 @@ typedef void (*ssd1309_spi_handle)(uint8_t, uint8_t *, size_t);
 #ifndef SSD1309_OFFSET_Y
 #define SSD1309_OFFSET_Y        0
 #endif
+
+#define SSD1309_BUFFER_SIZE     (SSD1309_WIDTH * SSD1309_HEIGHT / 8)
 
 #define OLED_RESET              0
 #define OLED_WRITE_DATA         1
@@ -76,15 +78,34 @@ typedef enum
     White = 0x01  /* Pixel is set. Color depends on OLED  */
 } SSD1309_COLOR;
 
+typedef enum
+{
+    SSD1309_OK  = 0x00,
+    SSD1309_ERR = 0x01  /* Generic error                  */
+} SSD1309_Error_t;
+
+typedef enum
+{
+    ROTATION_0    = 0,
+    ROTATION_90   = 1,
+    ROTATION_180  = 2,
+    ROTATION_270  = 3
+} SSD1309_ROTATION;
+
 /* Struct to store transformations			  */
 typedef struct 
 {
     uint16_t CurrentX;
     uint16_t CurrentY;
-    uint8_t Inverted;
-    uint8_t Rotated;
+    SSD1309_ROTATION Rotation;
     uint8_t Initialized;
 } SSD1309_t;
+
+typedef struct
+{
+    uint8_t x;
+    uint8_t y;
+} SSD1309_VERTEX;
 
 
 /* Procedure definitions */
@@ -93,20 +114,26 @@ void ssd1309_Init(ssd1309_i2c_handle i2c_comm_handle);
 #elif defined(SSD1309_USE_SPI)
 void ssd1309_Init(ssd1309_spi_handle spi_comm_handle);
 #endif
+void ssd1309_SetContrast(const uint8_t value);
+
 void ssd1309_Fill(SSD1309_COLOR color);
 void ssd1309_UpdateScreen(void);
-bool ssd1309_Rotated(void);
-void ssd1309_RotatedText(void);
-void ssd1309_NonrotatedText(void);
 void ssd1309_DrawPixel(uint8_t x, uint8_t y, SSD1309_COLOR color);
 void ssd1309_WriteSymbol(SymbolID_t Symbol, uint8_t x, uint8_t y);
 char ssd1309_WriteChar(char ch, FontDef Font, SSD1309_COLOR color);
 char ssd1309_WriteString(char* str, FontDef Font, SSD1309_COLOR color);
 void ssd1309_SetCursor(uint8_t x, uint8_t y);
+void ssd1309_DrawLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1309_COLOR color);
+void ssd1309_DrawArc(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep, SSD1309_COLOR color);
+void ssd1309_DrawCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, SSD1309_COLOR color);
+void ssd1309_Polyline(const SSD1309_VERTEX *par_vertex, uint16_t par_size, SSD1309_COLOR color);
+void ssd1309_DrawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1309_COLOR color);
+void ssd1309_DrawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, SSD1309_COLOR color);
 
 /* Low-level procedures	*/
 void ssd1309_Reset(void);
 void ssd1309_WriteCommand(uint8_t byte);
 void ssd1309_WriteData(uint8_t* buffer, size_t buff_size);
+SSD1309_Error_t ssd1309_FillBuffer(uint8_t *buf, uint32_t len);
 
 #endif /* __SSD1309_H__	*/
